d8c0f05db069e02b8b2aaba28000007b4f4b6b23
Leandro Salustriano <leandro.salustriano@hotmail.com>
2023-09-17 14:29:06 -0300

    added to show total all invest/profit

--- src/usecases/transactions/cost-by-group-transactions-io.usecase.ts
+++ src/usecases/transactions/cost-by-group-transactions-io.usecase.ts
@@ -10,73 +10,99 @@ import { MainContractsModel } from "../../model/main-contracts.model";
 
 //TODO corrigir tipagem do agroupping posteriormente
 interface CostByGroupTransactionsIOUseCaseRequest {
-  groups: any,
-  transaction_io: TransactionIOModel,
-  main_contract: MainContractsModel
+  groups: any;
+  transaction_io: TransactionIOModel;
+  main_contract: MainContractsModel;
 }
 
 class CostByGroupTransactionsIOUseCase {
   constructor(
     private web3Repository: Web3Interface,
     private requestsRepository: RequestsInterface
-  ) { }
+  ) {}
 
-  async exec({ groups, transaction_io, main_contract }: CostByGroupTransactionsIOUseCaseRequest) {
-
-    const cost_groups: any = []
-    const tokens_selled_by_group: any = []
-    let total_bought = BigInt(0)
+  async exec({
+    groups,
+    transaction_io,
+    main_contract,
+  }: CostByGroupTransactionsIOUseCaseRequest) {
+    const cost_groups: any = [];
+    const tokens_selled_by_group: any = [];
+    let total_bought = BigInt(0);
 
     for (var i in groups) {
       const index = parseInt(i);
-      cost_groups.push([])
+      cost_groups.push([]);
       tokens_selled_by_group.push(BigInt(0));
-      //tokens_bought_by_group.push(BigInt(0));      
+      //tokens_bought_by_group.push(BigInt(0));
 
       for (var _ in groups[index]) {
-        const group = groups[index][_] as EventLog & { operation: string }
+        const group = groups[index][_] as EventLog & { operation: string };
 
-        const internal_transactions = await (new InternalTransactionsUseCase(this.requestsRepository)).exec({ transactionHash: group.transactionHash })
-        const internal_cost_group = new InternalCostTransactionsUseCase(this.web3Repository).exec({ internal_transactions });
+        const internal_transactions = await new InternalTransactionsUseCase(
+          this.requestsRepository
+        ).exec({ transactionHash: group.transactionHash });
+        const internal_cost_group = new InternalCostTransactionsUseCase(
+          this.web3Repository
+        ).exec({ internal_transactions });
 
         if (group.operation == "buy") {
-          cost_groups[index].bought = group.args[2]
-          cost_groups[index].bought_hash = group.transactionHash
+          cost_groups[index].bought = group.args[2];
+          cost_groups[index].bought_hash = group.transactionHash;
 
-          cost_groups[index].bribe = internal_transactions[0].to != env.WETH ? Number(internal_transactions[0].value) : 0
-          cost_groups[index].total_cost_transaction = ethers.parseEther('0') - internal_cost_group.received
+          cost_groups[index].bribe =
+            internal_transactions[0].to != env.WETH
+              ? Number(internal_transactions[0].value)
+              : 0;
+          cost_groups[index].total_cost_transaction =
+            ethers.parseEther("0") - internal_cost_group.received;
 
-          const tokens_sell_transaction = transaction_io.contract_transactions?.contract_transactions_in.filter(transaction => transaction.transactionHash == group.transactionHash) as EventLog[]
+          const tokens_sell_transaction =
+            transaction_io.contract_transactions?.contract_transactions_in.filter(
+              (transaction) =>
+                transaction.transactionHash == group.transactionHash
+            ) as EventLog[];
 
-          total_bought = tokens_sell_transaction[0].args[2] + total_bought
+          total_bought = tokens_sell_transaction[0].args[2] + total_bought;
         } else {
-          cost_groups[index].sell_hash = group.transactionHash
+          cost_groups[index].sell_hash = group.transactionHash;
 
-          if (!cost_groups[index].total_sell) cost_groups[index].total_sell = internal_cost_group.selling
-          else cost_groups[index].total_sell += internal_cost_group.selling
+          if (!cost_groups[index].total_sell)
+            cost_groups[index].total_sell = internal_cost_group.selling;
+          else cost_groups[index].total_sell += internal_cost_group.selling;
 
-          const current_tokens = await main_contract.token_contract.balanceOf(env.USER_ADDRESS, { blockTag: group.blockNumber })
-          total_bought -= current_tokens
-          tokens_selled_by_group[index] += total_bought
-          cost_groups[index].tokens_sell = tokens_selled_by_group[index]
+          const current_tokens = await main_contract.token_contract.balanceOf(
+            env.USER_ADDRESS,
+            { blockTag: group.blockNumber }
+          );
+          total_bought -= current_tokens;
+          tokens_selled_by_group[index] += total_bought;
+          cost_groups[index].tokens_sell = tokens_selled_by_group[index];
         }
 
-        const transaction = await this.web3Repository.getTransaction(group.transactionHash)
-        const transaction_receipt = await this.web3Repository.getTransactionReceipt(group.transactionHash)
+        const transaction = await this.web3Repository.getTransaction(
+          group.transactionHash
+        );
+        const transaction_receipt =
+          await this.web3Repository.getTransactionReceipt(
+            group.transactionHash
+          );
 
-        if (!cost_groups[index].total_cost_transaction) cost_groups[index].total_cost_transaction = transaction.value
+        if (!cost_groups[index].total_cost_transaction)
+          cost_groups[index].total_cost_transaction = transaction.value;
         else cost_groups[index].total_cost_transaction += transaction.value;
 
-        if (!cost_groups[index].total_gasfee) cost_groups[index].total_gasfee = transaction_receipt.gasUsed * transaction_receipt.gasPrice
-        else cost_groups[index].total_gasfee += transaction_receipt.gasUsed * transaction_receipt.gasPrice;
-
+        if (!cost_groups[index].total_gasfee)
+          cost_groups[index].total_gasfee =
+            transaction_receipt.gasUsed * transaction_receipt.gasPrice;
+        else
+          cost_groups[index].total_gasfee +=
+            transaction_receipt.gasUsed * transaction_receipt.gasPrice;
       }
-
     }
 
-    return cost_groups
+    return cost_groups;
   }
-
 }
 
-export { CostByGroupTransactionsIOUseCase }
+export { CostByGroupTransactionsIOUseCase };
--- src/usecases/transactions/message-format-transactions-io.usecase.ts
+++ src/usecases/transactions/message-format-transactions-io.usecase.ts
@@ -6,58 +6,70 @@ import { Web3Interface } from "../../repository/interfaces/web3.interface";
 import { LocatePersonalUnitUtils } from "../../utils/locate-personal-unit.utils";
 
 interface MessageFormatTransactionsIOUseCaseRequest {
-  token_info:TokenInfoModel
-  formated_transactions_group:any[],
-  contract_address:string
+  token_info: TokenInfoModel;
+  formated_transactions_group: any[];
+  contract_address: string;
 }
 
 class MessageFormatTransactionsIOUseCase {
-  constructor(private web5Repository:Web3Interface){}
+  constructor(private web5Repository: Web3Interface) {}
 
-  async exec({token_info,formated_transactions_group,contract_address}:MessageFormatTransactionsIOUseCaseRequest){
-    let message = `${token_info.name}\n`
-    message += `${contract_address}\n\n`
-    //let message = ``
-    
-    formated_transactions_group.forEach(cost_group => {
-      const bought = parseFloat(cost_group.bought.eth).toFixed(5)
-      const approve = parseFloat(cost_group.approve.eth).toFixed(5)
-      const txgas = parseFloat(cost_group.total_gasfee.eth).toFixed(5)
-      
-      const total_spent = ( Number(cost_group.total_gasfee.eth) + Number(cost_group.total_cost_transaction.eth) + Number(cost_group.approve.eth)).toString()
-      const total_investiment = parseFloat(total_spent).toFixed(5)
-     
-      message += `Buy: ${env.BLOCKSCAN}${cost_group.bought.hash}\n`
+  async exec({
+    token_info,
+    formated_transactions_group,
+    contract_address,
+  }: MessageFormatTransactionsIOUseCaseRequest) {
+    let message = `${token_info.name}\n`;
+    message += `${contract_address}\n\n`;
+    let total_all_cost = 0;
+    let total_all_profit = 0;
 
-      if(cost_group.total_sell.hash) {
+    formated_transactions_group.forEach((cost_group) => {
+      const bought = parseFloat(cost_group.bought.eth).toFixed(5);
+      const approve = parseFloat(cost_group.approve.eth).toFixed(5);
+      const txgas = parseFloat(cost_group.total_gasfee.eth).toFixed(5);
+
+      const total_spent =
+        Number(cost_group.total_gasfee.eth) +
+        Number(cost_group.total_cost_transaction.eth) +
+        Number(cost_group.approve.eth);
+      const total_investiment = parseFloat(total_spent.toString()).toFixed(5);
+
+      message += `Buy: ${env.BLOCKSCAN}${cost_group.bought.hash}\n`;
+
+      if (cost_group.total_sell.hash) {
         message += `Sell: ${env.BLOCKSCAN}${cost_group.total_sell.hash}\n\n`;
       } else {
         message += `\n`;
-      }        
-
-      message += `Token Investiment: ${bought}\n`  
-      message += `Approve: ${approve}\n`
-      message += `Bribe: ${cost_group.bribe.eth}\n`
-      message += `TxGas: ${txgas}\n\n`      
-      
-      message += `Total investiment: ${total_investiment}\n`
-      if(cost_group.total_sell.eth){        
-        const profit = (Number(cost_group.total_sell.eth) - Number(total_spent)).toString()
-        const tokens_selled = LocatePersonalUnitUtils(cost_group.tokens_sell.wei,token_info.decimals as number)
-        const eth_selled = parseFloat(cost_group.total_sell.eth).toFixed(5)
-        message += `Token Sell: ${eth_selled}\n`
-        const format_profit = parseFloat(profit).toFixed(5)
-        message += `Profit: ${format_profit}\n\n`
-        
-      } else {
-        message += `\n`
       }
 
+      message += `Token Investiment: ${bought}\n`;
+      message += `Approve: ${approve}\n`;
+      message += `Bribe: ${cost_group.bribe.eth}\n`;
+      message += `TxGas: ${txgas}\n\n`;
+
+      message += `Total investiment: ${total_investiment}\n`;
+      if (cost_group.total_sell.eth) {
+        const profit = Number(cost_group.total_sell.eth) - Number(total_spent);
+        const tokens_selled = LocatePersonalUnitUtils(
+          cost_group.tokens_sell.wei,
+          token_info.decimals as number
+        );
+        const eth_selled = parseFloat(cost_group.total_sell.eth).toFixed(5);
+        message += `Token Sell: ${eth_selled}\n`;
+        const format_profit = parseFloat(profit.toString()).toFixed(5);
+        message += `Profit: ${format_profit}\n\n`;
+        total_all_profit += profit;
+      } else {
+        message += `\n`;
+      }
+      total_all_cost += total_spent;
     });
+    message += `Total all investment: ${total_all_cost}\n`;
+    message += `Total all profit: ${total_all_profit}`;
 
     return message;
   }
-
 }
 
-export {MessageFormatTransactionsIOUseCase}
+export { MessageFormatTransactionsIOUseCase };
